da8755b22bf55b14212895ed46590750f12b7061
artizarif <102957637+artizarif@users.noreply.github.com>
2022-05-30 16:09:44 +0530

    task007

---
+++ script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".card-slider", {
+    grabCursor: true,
+    loop: true,
+    centeredSlides:true,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+    }
+});
